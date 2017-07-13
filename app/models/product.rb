5eb8064ffd0a9912f94be0b56482688ec1e1ebf2
Jonathan Martinez <jonathan.martinez1217@gmail.com>
2017-07-12 21:28:57 -0500

    adds tax and total

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,4 +1,18 @@
 class Product < ApplicationRecord
+  def sale_message
+    if price <= 2
+      message = "Discount Item!"
+    else 
+      message = "Everyday Value!"
+    end
+  end 
 
+  def tax 
+    price * 0.09
+  end 
+
+  def total 
+    price + tax
+  end 
   
 end
