54e3c6fe9cd9caf2aa0402a3ab911ea4c8f0885b
Jonathan Martinez <jonathan.martinez1217@gmail.com>
2017-08-03 09:45:24 -0500

    modified user controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,19 +4,18 @@ class UsersController < ApplicationController
   end
 
   def create
-    user = User.new(
+    @user = User.new(
       name: params[:name],
       email: params[:email],
       password: params[:password],
       password_confirmation: params[:password_confirmation]
     )
-    if user.save
-      session[:user_id] = user.id
+    if @user.save
+      session[:user_id] = @user.id
       flash[:success] = 'Successfully created account!'
       redirect_to '/'
     else
-      flash[:warning] = 'Invalid email or password!'
-      redirect_to '/signup'
+      render "new.html.erb"
     end
   end
 
--- app/models/user.rb
+++ app/models/user.rb
@@ -3,5 +3,9 @@ class User < ApplicationRecord
   has_many :orders
   has_many :carted_products
   has_many :prdoduct ,through: :carted_products
+
+  validates(:name, {:uniqueness => true, :presence => true})
+  validates(:email, {:uniqueness => true, :presence => true})
+  validates(:password_digest,{:presence => true})
 end
 
